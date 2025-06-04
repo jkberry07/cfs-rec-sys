f04c0081e002c29b1238f309aca39cf317393692
Kendall Berry <jkendall.berry@gmail.com>
2025-06-04 16:19:31 -0700

    Moved the nltk download from server.py to split_sent function

--- server.py
+++ server.py
@@ -108,12 +108,3 @@ def recommendations():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 7000))
     app.run(host='0.0.0.0', port=port)
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except LookupError:
-        nltk.download('punkt')
-
-    try:
-        nltk.data.find('tokenizers/punkt_tab')
-    except LookupError:
-        nltk.download('punkt_tab')
